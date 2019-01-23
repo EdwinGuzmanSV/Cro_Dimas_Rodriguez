67e04a772ea2f7eb9f3727ad40878c9fa2170fe0
Edwin Otoniel Guzman <edwinotoguzman69@hotmail.com>
2019-01-22 21:53:54 -0600

    agregadas anectotas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                             <li><a href="{{ route('tags.index') }}">Etiquetas</a></li>
                             <li><a href="{{ route('categories.index') }}">Categorías</a></li>
                             <li><a href="{{ route('faqs.index') }}">FAQs</a></li>
-                            <li><a href="{{ route('posts.index') }}">Anectotas</a></li>
+                            <li><a href="{{ route('anecdotas.index') }}">Anectotas</a></li>
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
