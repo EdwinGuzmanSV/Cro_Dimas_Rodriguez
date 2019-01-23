7bc89c2e772bebf5e222304c0d97a8854ec9417d
AlexisLopez350 <39870674+AlexisLopez350@users.noreply.github.com>
2019-01-22 21:48:18 -0600

    Solucionado link anecdotas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                         @else
                             <li><a href="{{ route('tags.index') }}">Etiquetas</a></li>
                             <li><a href="{{ route('categories.index') }}">Categorías</a></li>
-                            <li><a href="{{ route('posts.index') }}">Anectotas</a></li>
+                            <li><a href="{{ route('anecdotas.index') }}">Anectotas</a></li>
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
